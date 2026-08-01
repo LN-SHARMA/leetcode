684e86f813041b5710a610d099daad62587995b3
Laxmi Narayan Sharma <lns22505@gmail.com>
2026-08-01 23:38:20 +0530

    Time: 41 ms (54.75%), Space: 66.6 MB (65.78%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+
+        int MOD = 1000000007;
+
+        int[][] engineers = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            engineers[i][0] = efficiency[i];
+            engineers[i][1] = speed[i];
+        }
+
+        Arrays.sort(engineers, (a, b) -> b[0] - a[0]);
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        long speedSum = 0;
+        long maxPerformance = 0;
+
+        for (int[] engineer : engineers) {
+
+            int currEfficiency = engineer[0];
+            int currSpeed = engineer[1];
+
+            minHeap.offer(currSpeed);
+            speedSum += currSpeed;
+
+            if (minHeap.size() > k) {
+                speedSum -= minHeap.poll();
+            }
+
+            maxPerformance = Math.max(maxPerformance,
+                                      speedSum * currEfficiency);
+        }
+
+        return (int)(maxPerformance % MOD);
+    }
+}
